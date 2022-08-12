e66c9400ea1fe9cf898fff1d20d6dc51d6d79e69
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-12 10:01:15 -0700

    Allow anonymous viewing on the home page

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using PierresTreats.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace PierresTreats.controller
 {
   public class HomeController : Controller
   {
+    private readonly PierresTreatsContext _db;
+    public HomeController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+    
+    [AllowAnonymous]
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.Title = "Pierre's Sweets and Treats";
+      ViewBag.Subtitle = "Savory and Sweet For Any Moment";
+      ViewBag.FlavorTreat = _db.FlavorTreat;
       return View();
     }
   }
