642b0963d9a7887e1c11abd27f99a2f731a30e65
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-12 09:18:28 -0700

    Add using directives

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -4,22 +4,26 @@ using Microsoft.AspNetCore.Mvc;
 using PierresTreats.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace PierresTreats.Controllers
 {
-  public class TreatController : Controller
-  {
-    private readonly PierresTreatsContext _db;
+  // public class TreatController : Controller
+  // {
+  //   private readonly PierresTreatsContext _db;
 
-    public CuisineController(PierresTreatsContext db)
-    {
-      _db = db;
-    }
+  //   public TreatController(PierresTreatsContext db)
+  //   {
+  //     _db = db;
+  //   }
 
-    public ActionResult Index()
-    {
-      List<Treat> model = _db.Treat.ToList();
-      return View(model);
-    }
-  }
+  //   public ActionResult Index()
+  //   {
+  //     List<Treat> model = _db.Treat.ToList();
+  //     return View(model);
+  //   }
+  // }
 }
