6bb9d541a9fd083da90d9693f37d0bce0462b721
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-12 16:09:55 -0700

    Add Authorize to class

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Security.Claims;
 
 namespace PierresTreats.Controllers
-{
+{ [Authorize]
   public class TreatsController : Controller
   {
     private readonly PierresTreatsContext _db;
@@ -102,25 +102,25 @@ namespace PierresTreats.Controllers
       return RedirectToAction("Index");
     }
     
-    public ActionResult AddTreat(int id)
-    {
-      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
-      ViewBag.Title ="Treats";
-      ViewBag.Subtitle = "Add Flavor to " + thisTreat.Name;
-      ViewBag.SelectList(_db.Treats, "TreatId", "Name");
-      return View(thisTreat);
-    }
+    // public ActionResult AddFlavor(int id)
+    // {
+    //   var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+    //   ViewBag.Title ="Treats";
+    //   ViewBag.Subtitle = "Add Flavor to " + thisTreat.Name;
+    //   ViewBag.SelectList(_db.Treats, "TreatId", "Name");
+    //   return View(thisTreat);
+    // }
     
-    [HttpPost]
-    public ActionResult AddTreat(Treat treat, int FlavorId)
-    {
-      if (FlavorId != 0)
-      {
-        _db.FlavorTreat.Add(new FlavorTreat() {FlavorId = FlavorId, TreatId = treat.TreatId});
-        _db.SaveChanges();
-      }
-      return RedirectToAction("Index");
-    }
+    // [HttpPost]
+    // public ActionResult AddFlavor(Treat treat, int FlavorId)
+    // {
+    //   if (FlavorId != 0)
+    //   {
+    //     _db.FlavorTreat.Add(new FlavorTreat() {FlavorId = FlavorId, TreatId = treat.TreatId});
+    //     _db.SaveChanges();
+    //   }
+    //   return RedirectToAction("Index");
+    // }
     
     [HttpPost]
     public ActionResult DeleteFlavor(int joinId)
