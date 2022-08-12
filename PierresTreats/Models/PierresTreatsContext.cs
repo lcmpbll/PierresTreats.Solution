ecae7c27fe144e04aac5b61595dd479d7985bcab
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-12 10:22:45 -0700

    Pluralize dbset names

--- PierresTreats/Models/PierresTreatsContext.cs
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -5,8 +5,8 @@ namespace PierresTreats.Models
 {
   public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
   {
-    public DbSet<Flavor> Flavor { get; set; }
-    public DbSet<Treat> Treat { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
     public DbSet<FlavorTreat> FlavorTreat { get;  set; }
 
     public PierresTreatsContext(DbContextOptions options) : base(options) { }
